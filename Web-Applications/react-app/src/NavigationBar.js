d1e3b2329e74db77c24a2ab0742f961fd7ba8ffd
Axel Vandebrouck <axel@smood.ch>
2019-03-26 16:16:31 +0100

    nav bar complete

--- Web-Applications/react-app/src/NavigationBar.js
+++ Web-Applications/react-app/src/NavigationBar.js
@@ -1,149 +1,62 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import InputBase from '@material-ui/core/InputBase';
-import { fade } from '@material-ui/core/styles/colorManipulator';
-import { withStyles } from '@material-ui/core/styles';
-import SearchIcon from '@material-ui/icons/Search';
-import Link from "@material-ui/core/Link";
-import SvgIcon from '@material-ui/core/SvgIcon';
-import IconButton from "@material-ui/core/IconButton";
+import './css/Pages.css'
 
-const styles = theme => ({
-  root: {
-    width: '100%',
-  },
-  appBar: {
-    backgroundColor: '#333',
-  },
-  grow: {
-    flexGrow: 1,
-  },
-  gitButton: {
-    backgroundColor: 'transparent',
-  },
-  linkText: {
-    display: 'none',
-    [theme.breakpoints.up('sm')]: {
-      display: 'block',
-    },
-    textDecoration: 'none',
-    color: '#fff',
-    '&:hover': {
-      textDecoration: 'none',
-      color: '#aaa',
-    },
-    marginRight: 5 * theme.spacing.unit,
-  },
-  search: {
-    position: 'relative',
-    borderRadius: theme.shape.borderRadius,
-    backgroundColor: fade(theme.palette.common.white, 0.15),
-    '&:hover': {
-      backgroundColor: fade(theme.palette.common.white, 0.25),
-    },
-    marginLeft: 0,
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-      marginLeft: theme.spacing.unit,
-      width: 'auto',
-    },
-  },
-  searchIcon: {
-    width: theme.spacing.unit * 9,
-    height: '100%',
-    position: 'absolute',
-    pointerEvents: 'none',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  inputRoot: {
-    color: 'inherit',
-    width: '100%',
-  },
-  inputInput: {
-    paddingTop: 0, //2 * theme.spacing.unit,
-    paddingRight: theme.spacing.unit,
-    paddingBottom: 0, //2 * theme.spacing.unit,
-    paddingLeft: theme.spacing.unit * 9,
-    transition: theme.transitions.create('width'),
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-      width: 400,
-      '&:focus': {
-        width: 420,
-      },
-    },
-    height: 40,
-    fontSize: 16,
-  },
-});
+import React, {Component} from 'react';
+import {Navbar, Nav, NavItem, NavDropdown, MenuItem, FormControl, Form} from 'react-bootstrap';
+
+import {LinkContainer} from 'react-router-bootstrap'
+import SvgIcon from "@material-ui/core/SvgIcon";
 
 const GitHubIcn = () => (
-    <SvgIcon nativeColor={'white'}>
-      <path d="M12.007 0C6.12 0 1.1 4.27.157 10.08c-.944 5.813 2.468 11.45 8.054 13.312.19.064.397.033.555-.084.16-.117.25-.304.244-.5v-2.042c-3.33.735-4.037-1.56-4.037-1.56-.22-.726-.694-1.35-1.334-1.756-1.096-.75.074-.735.074-.735.773.103 1.454.557 1.846 1.23.694 1.21 2.23 1.638 3.45.96.056-.61.327-1.178.766-1.605-2.67-.3-5.462-1.335-5.462-6.002-.02-1.193.42-2.35 1.23-3.226-.327-1.015-.27-2.116.166-3.09 0 0 1.006-.33 3.3 1.23 1.966-.538 4.04-.538 6.003 0 2.295-1.5 3.3-1.23 3.3-1.23.445 1.006.49 2.144.12 3.18.81.877 1.25 2.033 1.23 3.226 0 4.607-2.805 5.627-5.476 5.927.578.583.88 1.386.825 2.206v3.29c-.005.2.092.393.26.507.164.115.377.14.565.063 5.568-1.88 8.956-7.514 8.007-13.313C22.892 4.267 17.884.007 12.008 0z" />
-    </SvgIcon>
-  );
+  <SvgIcon nativeColor={'white'}>
+    <path d="M12.007 0C6.12 0 1.1 4.27.157 10.08c-.944 5.813 2.468 11.45 8.054 13.312.19.064.397.033.555-.084.16-.117.25-.304.244-.5v-2.042c-3.33.735-4.037-1.56-4.037-1.56-.22-.726-.694-1.35-1.334-1.756-1.096-.75.074-.735.074-.735.773.103 1.454.557 1.846 1.23.694 1.21 2.23 1.638 3.45.96.056-.61.327-1.178.766-1.605-2.67-.3-5.462-1.335-5.462-6.002-.02-1.193.42-2.35 1.23-3.226-.327-1.015-.27-2.116.166-3.09 0 0 1.006-.33 3.3 1.23 1.966-.538 4.04-.538 6.003 0 2.295-1.5 3.3-1.23 3.3-1.23.445 1.006.49 2.144.12 3.18.81.877 1.25 2.033 1.23 3.226 0 4.607-2.805 5.627-5.476 5.927.578.583.88 1.386.825 2.206v3.29c-.005.2.092.393.26.507.164.115.377.14.565.063 5.568-1.88 8.956-7.514 8.007-13.313C22.892 4.267 17.884.007 12.008 0z" />
+  </SvgIcon>
+);
 
-function SearchAppBar(props) {
-  const { classes } = props;
-  return (
-    <div className={classes.root}>
-      <AppBar className={classes.appBar}>
-        <Toolbar>
-          {/*<IconButton className={classes.menuButton} color="inherit" aria-label="Open drawer">
-            <MenuIcon />
-          </IconButton>*/}
-          <Typography variant="h4" color="inherit" style={{marginRight:20}}>
-            <Link href="/" className={classes.linkText}>
-              DNAM
-            </Link>
-          </Typography>
+/*---------------------------------------------------------------------------------- DONE ----------------------------------------------------------------------------------*/
+//TODO : Add About, Additional INfo and API
+/*
+* Class representing the Navigation Bar component
+* */
+export default class NavigationBar extends Component {
 
-          <Typography variant="h6" color="inherit" style={{ display: 'flex', flexDirection: 'row' }}>
-            <Link href="/DepositFile" className={classes.linkText}>
-              NEW DEPOSIT
-            </Link>
-            <Link href="/MyFiles" className={classes.linkText}>
-              MY FILES
-            </Link>
-            <Link href="/Store" className={classes.linkText}>
-              STORE
-            </Link>
-            <Link href="/MyRequests" className={classes.linkText}>
-              REQUESTS
-            </Link>
-            <Link href="/About" className={classes.linkText}>
-              ABOUT US
-            </Link>
-          </Typography>
-          <div className={classes.grow} />
-          <div className={classes.search}>
-            <div className={classes.searchIcon}>
-              <SearchIcon />
-            </div>
-            <InputBase
-              placeholder="Find your next tube..."
-              classes={{
-                root: classes.inputRoot,
-                input: classes.inputInput,
-              }}
-            />
-          </div>
-          <IconButton href="https://github.com/avandebr/DNAM" style={{marginLeft:10}}>
-            <GitHubIcn/>
-          </IconButton>
-        </Toolbar>
-      </AppBar>
-    </div>
-  );
+  render() {
+    return (
+      <Navbar inverse collapseOnSelect fixedTop fluid className="navbar">
+        <Navbar.Header>
+          <Navbar.Brand>
+            <LinkContainer to="/"><a>DNAM</a></LinkContainer>
+          </Navbar.Brand>
+          <Navbar.Toggle/>
+        </Navbar.Header>
+        <Navbar.Collapse>
+          <Nav>
+            <NavDropdown title="Deposit" id="basic-nav-dropdown">
+              <LinkContainer to="/RegisterSong"><MenuItem>Song Registration</MenuItem></LinkContainer>
+              <LinkContainer to="/RegisterAlbum"><MenuItem>Album Registration</MenuItem></LinkContainer>
+            </NavDropdown>
+            <LinkContainer to="/Store"><MenuItem>Store</MenuItem></LinkContainer>
+            <LinkContainer to="/MyFiles"><MenuItem>My Files</MenuItem></LinkContainer>
+            <LinkContainer to="/MyRequests"><MenuItem>My Requests</MenuItem></LinkContainer>
+          </Nav>
+
+          <Nav>
+            <Form className="searchbar-container">
+              <FormControl type="text" placeholder="Search" className="searchbar"/>
+            </Form>
+          </Nav>
+
+          <Nav pullRight>
+
+            <LinkContainer to="/About">
+              <NavItem>About DNA</NavItem>
+            </LinkContainer>
+
+            <Navbar.Brand>
+              <NavItem href="https://github.com/avandebr/DNAM"><GitHubIcn/></NavItem>
+            </Navbar.Brand>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    );
+  }
 }
-
-SearchAppBar.propTypes = {
-  classes: PropTypes.object.isRequired,
-};
-
-export default withStyles(styles)(SearchAppBar);
---
+++ Web-Applications/react-app/src/Patenting/DepositFolder.js
@@ -0,0 +1,306 @@
+import '../css/Pages.css'
+
+import React, {Component} from 'react';
+import {Grid, Row, Col} from 'react-bootstrap';
+import {EncryptFileButton, FieldGroup, SubmitButton, ContractNotFound} from '../utils/FunctionalComponents';
+import {validateEmail, validateFile} from '../utils/UtilityFunctions';
+import {getFileHash} from '../utils/CryptoUtils'
+import wrapWithMetamask from '../MetaMaskWrapper'
+import Patenting from '../../build/contracts/Patenting';
+import Bundle from '../utils/ipfsBundle';
+
+import {Constants, FileStates} from '../utils/Constants';
+import {generatePrivateKey} from '../utils/KeyGenerator'
+import {INVALID_FORM, KEY_GENERATION_ERROR, IPFS_ERROR, contractError} from '../utils/ErrorHandler'
+import Dialog from 'react-bootstrap-dialog';
+import Paper from "@material-ui/core/Paper";
+import Divider from "@material-ui/core/Divider";
+
+/*---------------------------------------------------------------------------------- DONE ----------------------------------------------------------------------------------*/
+
+
+/*Component for Patent Deposit*/
+class DepositAlbum_class extends Component {
+
+  /*Component Constructor*/
+  constructor(props) {
+    super(props);
+    this.bundle = new Bundle();
+    this.state = {
+      hash: "",
+      ipfsLocation: "",
+      patentName: "",
+      price: "",
+      email_address: "",
+      repeat_email: "",
+      file: "",
+      fileState: FileStates.NOT_ENCRYPTED,
+      web3: props.web3,
+      contractInstance: null,
+      waitingTransaction: false,
+      patentPrice: 0,
+      etherPrice : 0,
+      gasPrice : 0
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.submitFile = this.submitFile.bind(this);
+    this.encryptFile = this.encryptFile.bind(this);
+  }
+
+  /* Called before the component is mounted
+  * Instantiates the contract and stores the price of a patent */
+  componentDidMount() {
+    this.state.web3.eth.getGasPrice((err, res) => this.setState({ gasPrice : res.toNumber() }));
+    const contract = require('truffle-contract');
+    const patenting = contract(Patenting);
+    patenting.setProvider(this.state.web3.currentProvider);
+    // patenting.at('0x90Aa08D8542925bc95b1D7347bF21068A0659c70').then(instance => { // for ROPSTEN
+    // patenting.at('0xddfc2E31EEcA6Ed9E39ed4B7BA30F7217B3032A3').then(instance => { // for LOCAL RPC
+    patenting.deployed().then(instance => { // for LOCAL RPC
+      this.setState({contractInstance: instance});
+      return instance.patentPrice.call()
+    }).then(price => {
+      this.setState({patentPrice: price.toNumber()});
+      return this.state.contractInstance.getEthPrice.call(price.toNumber())
+    }).then(ethPrice => this.setState({ etherPrice: ethPrice }))
+      .catch(error => this.setState({ contractInstance: null }));
+  }
+
+
+  /*--------------------------------- HELPER METHODS AND VALIDATION ---------------------------------*/
+
+  /*Resets the form*/
+  resetForm() {
+    this.bundle.reset();
+    this.setState({
+      hash: "",
+      ipfsLocation: "",
+      patentName: "",
+      price: "",
+      file: "",
+      fileExt: '',
+      fileState: FileStates.NOT_ENCRYPTED,
+      email_address: "",
+      repeat_email: "",
+      waitingTransaction: false
+    })
+  }
+
+  /*Checks if Patent Name length is less than 100 */
+  validateName() {
+    let length = this.state.patentName.length;
+    if (length === 0) {
+      return null;
+    } else if (length <= 100) {
+      return "success"
+    } else {
+      return "error";
+    }
+  }
+
+  /*Checks that 0 <= price <= 1 ETHER*/
+  validatePrice() {
+    if (this.state.price === "") {
+      return null
+    } else if (!isNaN(this.state.price)) {
+      let price = parseInt(this.state.price, 10);
+      return (price <= 1000 && price >= 0 ? 'success' : 'warning');
+    } else {
+      return 'error'
+    }
+
+  }
+
+
+  /*Returns True if all form validation pass*/
+  validateForm() {
+    return (this.validatePrice() === 'success'
+      && this.validateName() === 'success'
+      && this.state.hash !== ""
+      && this.state.ipfsLocation !== ""
+      && this.state.fileState === FileStates.ENCRYPTED)
+  }
+
+
+  /*--------------------------------- EVENT HANDLERS ---------------------------------*/
+
+
+  /*Encrypts the file using AES and the key produced by the owner*/
+  encryptFile(e) {
+    e.preventDefault();
+    if (this.state.file !== "" && this.state.hash !== "" && this.state.fileState === FileStates.NOT_ENCRYPTED) {
+      this.setState({fileState: FileStates.ENCRYPTING});
+      generatePrivateKey(this.state.web3, this.state.hash).then(key => { // Ask user to generate key
+        return this.bundle.encryptFile(this.state.file, key); // Encrypt file using the key and return the IPFS hash of the result
+      }).then(files => {
+        this.setState({ipfsLocation: files[0].path, fileState: FileStates.ENCRYPTED})
+      }).catch(err => {
+        if (err === KEY_GENERATION_ERROR) {
+          window.dialog.showAlert(KEY_GENERATION_ERROR);
+        } else {
+          window.dialog.showAlert(IPFS_ERROR);
+          console.log(err);
+        }
+        this.resetForm();
+      })
+    } else {
+      window.dialog.showAlert("Please select a file.");
+    }
+
+  }
+
+
+  /* Handles the change in a form component
+  * Computes : sha256 of the plain text document
+  * */
+  handleChange(e) {
+    e.preventDefault();
+    if (e.target.name === Constants.FILE) {
+      console.log(e.target.files);
+      let file = e.target.files[0];
+      const fileName = file.name.split('.');
+      if (fileName.length > 2) {
+        window.dialog.showAlert("Invalid file name");
+      } else {
+        // default patent name is the file name
+        if (!this.state.patentName) {
+          this.setState({ patentName: fileName[0] });
+        }
+        this.setState({ fileExt: fileName[1] || 'mp3' });
+        if (validateFile(file)) {
+          this.setState({ waitingTransaction: true });
+          getFileHash(file, window).then(res => {
+            this.setState({ waitingTransaction: false, file: file, hash: res, fileState: FileStates.NOT_ENCRYPTED })
+          }).catch(err => window.dialog.showAlert(err));
+        }
+      }
+    }
+    else {
+      this.setState({ [e.target.name]: e.target.value });
+    }
+  }
+
+
+  /*Function that triggers the contract call to Deposit a patent*/
+  submitFile(e) {
+    e.preventDefault();
+    if (this.validateForm()) {
+      this.setState({waitingTransaction: true});
+      const { patentName, fileExt, hash, price, ipfsLocation, email_address } = this.state;
+      const completeName = patentName + '.' + fileExt;
+      this.state.contractInstance.depositPatent(completeName, hash, price, ipfsLocation, email_address, {
+        from: this.state.web3.eth.coinbase,
+        value: this.state.etherPrice,
+        gas: process.env.REACT_APP_GAS_LIMIT,
+        gasPrice : this.state.gasPrice
+      }).then(tx => {
+        return this.bundle.addFile() // Add the encrypted file to IPFS
+      }).then(filesAdded => {
+        this.resetForm();
+        window.dialog.show({
+          title: "Encrypted file has been successfully added to IPFS",
+          body: "IPFS location : ipfs.io/ipfs/" + filesAdded[0].path,
+          actions: [
+            Dialog.OKAction(),
+            Dialog.Action(
+              'View encrypted File',
+              () => {
+                let win = window.open("https://ipfs.io/ipfs/" + filesAdded[0].path);
+                win.focus();
+              })],
+          bsSize: "large"
+        });
+      }).catch(error => {
+        this.setState({waitingTransaction: false});
+        contractError(error); // Handles the error
+      });
+    } else {
+      if (this.state.file !== "" && this.state.fileState === FileStates.NOT_ENCRYPTED) {
+        window.dialog.showAlert("Please encrypt the file")
+      } else {
+        window.dialog.showAlert(INVALID_FORM);
+      }
+
+    }
+  }
+
+  /*--------------------------------- USER INTERFACE COMPONENTS ---------------------------------*/
+
+  /*The header to be displayed*/
+  static header() {
+    return (
+      <Grid>
+        <br/>
+        <Row bsClass='title'>Music Registration</Row>
+        <hr/>
+        <Row bsClass='paragraph'>
+          <p>This page allows users that have an Ethereum account and are using it on the Metamask
+            extension for browsers, to register files and allow other users to access them for a set
+            fee. <br/> Whenever another user requests to buy access to the file you uploaded, an email will be sent
+            to you and
+            you will need to <a href="/MyFiles" className="link">
+              accept the requests</a>, then the funds will be transferred to
+            your account and the user will be able to download a copy of the document.
+            <br/><br/>You only need to <b>unlock your Metamask extension</b> and choose the document.
+            <br/>Note that we do not store any data regarding the documents you upload; Only the hashes are retrieved.
+            The document will be stored in an encrypted format on the IPFS network, using AES 256-bit encryption
+          </p>
+        </Row>
+      </Grid>
+    );
+  }
+
+  /*Renders the form to deposit a patent*/
+  renderForm() {
+    return (
+      <Paper style={{ padding: 20 }}>
+        <form onSubmit={e => this.submitFile(e)}>
+          <FieldGroup name={Constants.FILE} id="formsControlsFile" label="File(s)" type="file" placeholder=""
+                      onChange={this.handleChange}/>
+          <FieldGroup name="patentName" id="formsControlsName" label="Patent Name (default will be files name)"
+                      type="text" value={this.state.patentName} placeholder="Enter the File name"
+                      help="Max 100 chars, without extension" validation={this.validateName()}
+                      onChange={this.handleChange} />
+          <EncryptFileButton fileState={this.state.fileState} onClick={e => this.encryptFile(e)}
+                             disabled={this.state.file === '' || this.state.fileState !== FileStates.NOT_ENCRYPTED} />
+          <br/><Divider/><br/>
+
+          <FieldGroup name="price" id="formsControlsName" label="Price in USD" type="text"
+                      value={this.state.price} help=""
+                      onChange={this.handleChange} validation={this.validatePrice()}/>
+          <FieldGroup name="email_address" id="formsControlsEmail" label="Email address" type="email"
+                      value={this.state.email_address} placeholder="john@doe.com" help=""
+                      onChange={this.handleChange}/>
+          <FieldGroup name="repeat_email" id="formsControlsEmail" label="Repeat Email address" type="email"
+                      value={this.state.repeat_email} placeholder="john@doe.com" help=""
+                      onChange={this.handleChange}
+                      validation={validateEmail(this.state.email_address, this.state.repeat_email)}/>
+
+          <Divider/><br/>
+          <SubmitButton running={this.state.waitingTransaction} disabled={!this.validateForm()}/>
+        </form>
+      </Paper>
+    );
+  }
+
+  render() {
+    if (this.state.contractInstance === null) {
+      return <ContractNotFound/>;
+    } else {
+      return (
+        <Grid>
+          <Row bsClass="contract-address">
+            <Col xsHidden>Contract at {this.state.contractInstance.address}</Col>
+            <Row>Deposit price at {this.state.patentPrice} USD </Row> {/* price to pay to deposit a patent */}
+            <br/> <Row><Col xsHidden>Current account {this.state.web3.eth.accounts[0]} (From Metamask)</Col></Row>
+          </Row>
+          <Row><Col sm={3} md={5} mdOffset={3} className="form">{this.renderForm()}</Col></Row>
+        </Grid>
+      )
+    }
+  }
+}
+
+const DepositAlbum = wrapWithMetamask(DepositAlbum_class, DepositAlbum_class.header());
+export default DepositAlbum;
+
