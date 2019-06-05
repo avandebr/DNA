86b9cf0e4ee21e4385b598c57a1b23864f2ee5ae
Axel Vandebrouck <axel@smood.ch>
2019-06-05 18:40:13 +0200

    cleaning for demo

--- Development-tools/test/contracts/HelloWorld.sol
+++
@@ -1,8 +0,0 @@
-contract HelloWorld {
-
-  //constant parameter : no state machine change in EVM
-  function displayMessage() constant returns (string) {
-    return "Hello from a smart contract";
-  }
-
-}
